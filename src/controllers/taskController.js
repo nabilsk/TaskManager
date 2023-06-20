3c517e08b8ba34ae00afe80a87ffdb314c2c086e
nabilsk <nabilshaikh7730@gmail.com>
2023-06-20 23:52:58 +0530

    added custom error handler

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,5 +1,6 @@
 const TaskDb = require("../models/TaskModel");
 const asyncWrapper = require("../middleare/async");
+const { createCustomError } = require("../errors/custom-error");
 
 const getAlltasks = asyncWrapper(async (req, res) => {
   const tasks = await TaskDb.find({});
@@ -18,33 +19,36 @@ const createTasks = asyncWrapper(async (req, res) => {
   });
 });
 
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await TaskDb.findOne({ _id: taskID });
   if (!task) {
-    return res.status(404).json({ msg: `No task found with id: ${taskID}` });
+    return next(createCustomError(`No task found with id: ${taskID}`, 404));
+    // return res.status(404).json({ msg: `No task found with id: ${taskID}` });
   }
 
   res.status(200).json({ task });
 });
 
-const updateTask = asyncWrapper(async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await TaskDb.findOneAndUpdate({ _id: taskID }, req.body, {
     new: true,
     runValidators: true,
   });
   if (!task) {
-    return res.status(404).json({ msg: `No task found with id: ${taskID}` });
+    return next(createCustomError(`No task found with id: ${taskID}`, 404));
+    // return res.status(404).json({ msg: `No task found with id: ${taskID}` });
   }
   res.status(200).json({ task });
 });
 
-const deleteTask = asyncWrapper(async (req, res) => {
+const deleteTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await TaskDb.findOneAndDelete({ _id: taskID });
   if (!task) {
-    return res.status(404).json({ msg: `No task found with id: ${taskID}` });
+    return next(createCustomError(`No task found with id: ${taskID}`, 404));
+    // return res.status(404).json({ msg: `No task found with id: ${taskID}` });
   }
 
   res.status(200).json({ msg: "succeess" });
